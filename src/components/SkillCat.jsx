000342cb9766000ecc3f2197188562b88a97cab7
mani759 <manikantagurram5533@gmail.com>
2026-06-20 23:16:42 +0530

    grid changed to mobile1

--- src/components/SkillCat.jsx
+++ src/components/SkillCat.jsx
@@ -38,7 +38,7 @@ const SkillCat = () => {
     },
   ];
   return (
-    <div className="grid grid-cols-1  md:grid-cols-2 gap-4 md:gap-8 w-full lg:max-w-[500px] ">
+    <div className="grid grid-cols-1  md:grid-cols-2 gap-4 md:gap-8 w-full  ">
       {Skills.map((cat, index) => {
         return (
           <motion.div
