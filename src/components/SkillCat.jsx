165be2170daf571a580c24333134e2245a9f9cc7
mani759 <manikantagurram5533@gmail.com>
2026-06-20 22:24:43 +0530

    changes saved

--- src/components/SkillCat.jsx
+++ src/components/SkillCat.jsx
@@ -38,7 +38,7 @@ const SkillCat = () => {
     },
   ];
   return (
-    <div className="grid grid-cols-2 gap-4 md:gap-8 w-full  max-w-[550px]">
+    <div className="grid grid-cols-2 gap-4 md:gap-8 w-full  max-w-[500px]">
       {Skills.map((cat, index) => {
         return (
           <motion.div
