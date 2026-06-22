94fc89ac8feb833bf439cbdfd7c36e7a35762fce
mani759 <manikantagurram5533@gmail.com>
2026-06-22 12:45:05 +0530

    chages saved

---
+++ src/components/CenterCard.jsx
@@ -0,0 +1,25 @@
+const CenterCard = () => {
+  return (
+    <div
+      className="
+        w-[420px]
+        h-[260px]
+
+        rounded-3xl
+
+        border border-white/10
+
+        bg-black/20
+        backdrop-blur-xl
+
+        flex
+        items-center
+        justify-center
+      "
+    >
+      <h2 className="text-white text-2xl font-bold">CENTER CARD</h2>
+    </div>
+  );
+};
+
+export default CenterCard;
--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { GridScan } from "../components/backgrounds/BgWhyMe";
 import OrbitCard from "../components/OrbitCard";
+import CenterCard from "../components/CenterCard";
 const About = () => {
   return (
-    <section id="about" className="relative min-h-screen overflow-hidden">
+    <section id="about" className="relative min-h-screen   overflow-hidden">
       <div className="absolute inset-0 opacity-30">
         <GridScan
           sensitivity={0.3}
@@ -25,14 +26,53 @@ const About = () => {
         <h2 className="text-white font-bold flex justify-center text-3xl mt-0">
           Why Me
         </h2>
-
-        <OrbitCard title="Card 1" description="Placeholder text" color="cyan" />
-
-        <OrbitCard title="Card Two" description="Testing" color="emerald" />
-        <OrbitCard title="Card 3" description="Testing" color="purple" />
-        <OrbitCard title="Card 4" description="Testing" color="amber" />
-        <OrbitCard title="Card 5" description="Testing" color="rose" />
-        <OrbitCard title="Card 6" description="Testing" color="blue" />
+        <div className="relative w-[900px]  h-[900px]   mx-auto">
+          <div className=" absolute  left-1/2  top-[45%]  -translate-x-1/2  -translate-y-1/2">
+            <CenterCard />
+          </div>
+          <div className="absolute top-[5%]  left-1/2 -translate-x-1/2">
+            <OrbitCard
+              title="Card 1"
+              description="Placeholder text"
+              color="cyan"
+            />
+          </div>
+          <div className="absolute top-[18%] left-0">
+            <OrbitCard
+              title="Card 2"
+              description="Placeholder text"
+              color="purple"
+            />
+          </div>
+          <div className="absolute top-[18%] right-0">
+            <OrbitCard
+              title="Card 3"
+              description="Placeholder text"
+              color="emerald"
+            />
+          </div>
+          <div className="absolute bottom-[18%] left-0">
+            <OrbitCard
+              title="Card 4"
+              description="Placeholder text"
+              color="amber"
+            />
+          </div>
+          <div className="absolute bottom-[18%] right-0">
+            <OrbitCard
+              title="Card 5"
+              description="Placeholder text"
+              color="rose"
+            />
+          </div>
+          <div className="absolute bottom-0 left-1/2 -translate-x-1/2">
+            <OrbitCard
+              title="Card 6"
+              description="Placeholder text"
+              color="blue"
+            />
+          </div>
+        </div>
       </div>
     </section>
   );
